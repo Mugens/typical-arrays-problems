810873d5f9bd6dfeb0ebc7c7cebdd8746308241f
vlad-herasymchuk <mugeens@gmail.com>
2021-03-10 16:18:38 +0200

    problems solved

--- src/index.js
+++ src/index.js
@@ -1,12 +1,25 @@
 
 exports.min = function min (array) {
-  return 0;
+  let arr = 0;
+  if(array && array.length>0){
+    arr = array.reduce((accum, curent)=>accum<curent ? accum : curent);
+  }
+  return arr;
 }
 
 exports.max = function max (array) {
-  return 0;
+  let arr = 0;
+  if(array && array.length>0){
+    arr = array.reduce((accum, curent)=>accum>curent ? accum : curent);
+  }
+  return arr;
 }
 
 exports.avg = function avg (array) {
-  return 0;
+  let arr = 0;
+  if(array && array.length>0){
+    arr = array.reduce((accum, curent)=>accum+curent);
+    arr= arr/array.length;
+  }
+  return arr;
 }
